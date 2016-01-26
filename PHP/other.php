47cd59d846194f9a60cbc18576d1a8597708bbcc
chriconn <connorchristensen4@gmail.com>
2016-01-26 02:13:01 -0800

    Names added to readme and css for php table
    The PHP table contains everyones names and onid account name, and I
    changed the CSS to make the website to display it on so it looks
    smoother.

--- PHP/other.php
+++ PHP/other.php
@@ -11,7 +11,7 @@
     </nav>
 
     <div>
-        <p id="description">This is a test website that was made for homework assignment #2 in the CS 290 course</p>
+        <p id="description">This is a test website that was made for homework assignment #3 in the CS 290 course</p>
     </div>
     <div>
         <p class="bold">People in the group</p>
@@ -40,14 +40,20 @@
             printf("Cannot create table(s).\n");
         }
 
-        $mysqli->query("insert into courses(cid,prof,cred,cap,title) values(123,'chriconn',4,70,'Connor')");
-        $mysqli->query("insert into courses(cid,prof,cred,cap,title) values(456,'shellhal',4,70,'Lily')");
-        $mysqli->query("insert into courses(cid,prof,cred,cap,title) values(789,'buffumw',4,70,'Billy')");
+        $mysqli->query("insert into courses(cid,prof,cred,cap,title) values(001,'chriconn',4,70,'Connor')");
+        $mysqli->query("insert into courses(cid,prof,cred,cap,title) values(010,'shellhal',4,70,'Lily')");
+        $mysqli->query("insert into courses(cid,prof,cred,cap,title) values(011,'buffumw',4,70,'Billy')");
+        $mysqli->query("insert into courses(cid,prof,cred,cap,title) values(100,'claytonh',4,70,'Heidi')");
+        $mysqli->query("insert into courses(cid,prof,cred,cap,title) values(101,'rosenber',4,70,'Robert')");
+        $mysqli->query("insert into courses(cid,prof,cred,cap,title) values(110,'prinei',4,70,'Ian')");
+        
+        
         $mysqli->query("insert into students(sid,onid,name) values(931905000,'chriconn','C. Christensen')");
         $mysqli->query("insert into students(sid,onid,name) values(931905001,'buffumw','B. Buffman')");
         $mysqli->query("insert into students(sid,onid,name) values(931905000,'shellhal','L. Shellhammer')");
 
-                echo "<table>";
+        echo "<table>";
+        echo "<h1>Mysqli table</h1>";
         if ($result = $mysqli->query("select cid,prof,cred,cap,title from courses")) {
             while($obj = $result->fetch_object()){ 
                     echo "<tr>";
