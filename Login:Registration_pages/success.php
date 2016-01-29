514d5012fdec104ff7708fa2e65fa4357f46362d
lilyshellhammer <shellhal@oregonstate.edu>
2016-01-28 21:13:28 -0800

    Adding login/registration pages
    I finished some basic css stuff on the login/registration/success pages
    Robert made.

---
+++ Login:Registration_pages/success.php
@@ -0,0 +1,9 @@
+
+<html>
+
+    <title>Sucess!</title>
+    <h1>SUCCESS</h1>
+    <p>You can now search for badges, check into locations using our geo-app to get badges, and view your badge profile!</p>
+    <link rel="stylesheet" type="text/css" href="./sucess.css">
+
+</html>
