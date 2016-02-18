5c7e831ce127584d0968a54628638d9051d2509f
ConnorChristensen <chriconn@onid.oregonstate.edu>
2016-02-17 16:19:11 -0800

    Documentation, CSS for login, image updates
    Updated the text file that contains links to all the websites where
    testing is being done. Improved the badge and converted to PNG with
    transparent background. Uploaded a file with some ideas about where to
    go from here.

--- login_registration/success.php
+++ login_registration/success.php
@@ -1,15 +1,23 @@
 <?php
 session_start();
 ?>
-<html>
+    <html>
 
-    <title>Success!</title>
-    <h1>SUCCESS</h1>
-    <p>You can now search for badges, check into locations using our geo-app to get badges, and view your badge profile!</p>
-    <link rel="stylesheet" type="text/css" href="./sucess.css">
-	<h3><?php //echo "<br>Session: " . $_SESSION["uid"] . "<br>";
+    <head>
+        <title>Success!</title>
+    </head>
+
+    <body>
+        <h1>SUCCESS</h1>
+        <p>You can now search for badges, check into locations using our geo-app to get badges, and view your badge profile!</p>
+        <link rel="stylesheet" type="text/css" href="./sucess.css">
+        <h3><?php //echo "<br>Session: " . $_SESSION["uid"] . "<br>";
 			echo "<br>Welcome, your session has been set<br>";
 		?>
 	</h3>
+        <div id="box">
+            <h2><a href="../index.html">HOME</a></h2>
+        </div>
+    </body>
 
-</html>
+    </html>
