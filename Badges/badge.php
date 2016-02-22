0b842d9d93139231f262e352595cbaf04c4646ae
ConnorChristensen <chriconn@onid.oregonstate.edu>
2016-02-21 21:04:10 -0800

    update badge page style and routing
    The website has something close to the final layout, more php is
    working and there have been some graphical improvements for the badges
    page.

--- Badges/badge.php
+++ Badges/badge.php
@@ -1,28 +1,36 @@
+<?php
+session_start();
+?>
 <html>
 <html>
-	<link rel="stylesheet" href="badge.css">
-    <script src="jquery-1.12.0.min.js"></script>
-    <script src="javaScript.js"></script>
+<link rel="stylesheet" href="badge.css">
+<script src="jquery-1.12.0.min.js"></script>
+<script src="javaScript.js"></script>
 </head>
 
 <script>
-images();
+    images();
 </script>
 
 <body>
-	<nav>
+    <nav>
         <table>
             <tr>
-                <td><img src="../Images/logo.png"></td>
-                <td><a href="../Map_Page/travel.html">Map</a></td>
-                <td>
-                	<div id="dropDown">
-                		<button id="dropDownClicker" onclick="dropDown()">USERNAME and IMAGE</button>
-                		<div id="dropDownHolder" class="hiddenDropDown">
-                			<button class="dropDownThings">account settings</button>
-                			<button class="dropDownThings">logout</button>
-                		</div>
-                	</div>
+                <td id="logo"><img src="../Images/IconWithoutBackground.png"></td>
+                <td id="toMap"><a href="../Map_Page/travel.html">Map</a></td>
+                <td id="dropDown">
+                    <div id="dropDownClicker" onclick="dropDown()">
+                       <?php 
+                        if(isset($_SESSION["login_user"])){
+                            echo $_SESSION["login_user"];
+                        }
+                        ?>
+                        <img src="../Images/empty_user.png" alt="">
+                    </div>
+                    <div class="hiddenDropDown" id="dropDownHolder">
+                        <div class="dropDownThings">account settings</div>
+                        <a class="dropDownThings" href="../index.html">logout</a>
+                    </div>
                 </td>
 
             </tr>
@@ -30,22 +38,22 @@ images();
     </nav>
 
 
-   <div id="banner">
-    	<div id="recentBadge" style="width: 100%; margin-top: 200px">
-    		<h1>You recently unlocked</h1>
-         </div>
+    <div id="banner">
+        <div id="recentBadge">
+            <h1>You recently unlocked</h1>
+        </div>
     </div>
 
-<!--Places the most recently unlocked badge-->
+    <!--Places the most recently unlocked badge-->
 
-<script>
-	mostRecentBadge();
-</script>
+    <script>
+        mostRecentBadge();
+    </script>
 
-    <div id="myModal" class="modal">
-    	<div id= "myModalContent" class="modal-content">
+    <div class="modal" id="myModal">
+        <div id="myModalContent" class="modal-content">
 
-    	</div>
+        </div>
     </div>
 
     <div id="badges">
@@ -54,10 +62,10 @@ images();
         </div>
     </div>
 
-<!--Populates the list of badges-->
-<script>
-	getBadges();  //populates the badges
-</script>
+    <!--Populates the list of badges-->
+    <script>
+        getBadges(); //populates the badges
+    </script>
 
 </body>
 
