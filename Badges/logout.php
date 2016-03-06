5bee9443cf15c57d7a160c5e2bf24498c1e83988
heidiaclayton <heidiaclayton@yahoo.com>
2016-03-06 10:51:21 -0800

    Update logout.php

--- Badges/logout.php
+++ Badges/logout.php
@@ -1,12 +1,11 @@
 <html>
 <h1>Logout Successful</h1>
-<h3>(Actually its not successful. The session is not being unset. Someone help.)</h3>
 <a href="../index.html">HOME</a>
 
 <?php
     session_set();
     session_destroy();
 ?>
-<meta http-equiv="refresh" content="0;url=http://web.engr.oregonstate.edu/~chriconn/index.html"/>
+<meta http-equiv="refresh" content="2;url=http://web.engr.oregonstate.edu/~chriconn/index.html"/>
 
 </html>
