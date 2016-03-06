22358da2f88ae59c5b7ed897f8a76e184bbc950e
Rob <rosenber@oregonstate.edu>
2016-03-06 11:03:55 -0800

    Moved the _header and linked everything to it

--- Database/_header.php
+++
@@ -1,5 +0,0 @@
-<?php
-//session_start();
-ini_set('display_errors', 'On'); 
-$con = mysqli_connect('oniddb.cws.oregonstate.edu','buffumw-db','PizSfykTJBUp3NbW','buffumw-db') or die("Can't connect to database.");
-?>
